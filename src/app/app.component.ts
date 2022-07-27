9b7427b58839cf8b096341efd70d0602e1ec950d
Muhammad Awais <64267313+Awais-mohammad@users.noreply.github.com>
2022-07-27 22:16:43 +0500

    intial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ammadfyp';
+  currentDiv: string;
+
+  constructor(
+    private papa: Papa
+  ) {
+    this.currentDiv = 'home'
+  }
+
+  showNoti(param) {
+    alert(param)
+  }
+
+  spin: boolean;
+  selectedCSVFileName: string;
+  isCSV_Valid: boolean;
+  data: any;
+
+  fileChangeListener($event: any): void {
+    this.spin = true
+    const files = $event.srcElement.files;
+
+    if (files !== null && files !== undefined && files.length > 0) {
+      this.selectedCSVFileName = files[0].name;
+
+      const reader: FileReader = new FileReader();
+      reader.readAsText(files[0]);
+      reader.onload = e => {
+
+        const csv = reader.result;
+        const results = this.papa.parse(csv as string, { header: false });
+
+        // VALIDATE PARSED CSV FILE
+        if (results !== null && results !== undefined && results.data !== null &&
+          results.data !== undefined && results.data.length > 0 && results.errors.length === 0) {
+          this.isCSV_Valid = true;
+
+          // PERFORM OPERATIONS ON PARSED CSV
+          let csvTableHeader = results.data[0];
+
+          let csvTableData = [...results.data.slice(1, results.data.length)];
+          this.data = csvTableData
+          this.data = this.data.slice(0, 100)
+
+          this.spin = false
+        } else {
+          for (let i = 0; i < results.errors.length; i++) {
+            this.showNoti('Error Parsing CSV File')
+            this.spin = false
+
+          }
+        }
+      };
+    } else {
+      this.showNoti('No File Selected')
+      this.spin = false
+
+    }
+
+  }
+
+
+  switchView(param) {
+    this.currentDiv = param
+  }
+  showAlert(param) {
+    alert(param)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PapaParseModule } from 'ngx-papaparse';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    PapaParseModule,
+    MatCheckboxModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
